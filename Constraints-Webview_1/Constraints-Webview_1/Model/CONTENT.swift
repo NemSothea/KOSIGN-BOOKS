dd923fae97b25520746f6e0cd1d031d99e48769d
Nem Sothea <nemsothea13@gmail.com>
2022-11-23 09:31:00 +0900

    Create Model CONTENT

---
+++ Constraints-Webview_1/Constraints-Webview_1/Model/CONTENT.swift
@@ -0,0 +1,31 @@
+//
+//  CONTENT.swift
+//  Constraints-Webview_1
+//
+//  Created by Bizplay on 2022/11/23.
+//
+
+import Foundation
+struct CONTENT {
+    
+    var title : String?
+    var type  : CONTENT_TYPE
+    
+}
+
+enum CONTENT_TYPE  {
+    
+    case InitWebView
+    case LoadURL
+    case WKNavigationDelegate
+    case Cookies
+    case LoadLocalHTML
+    case LoadLocalHTMLString
+    case LoadJavaScriptMethod
+    case InjectJavaScriptCode
+    case CallBackFromJavaScript
+    
+}
+
+
+
