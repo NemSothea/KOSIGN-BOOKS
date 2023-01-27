8dbe04594b9ba0aaf6395ddf4631f441527889f4
Nem Sothea <nemsothea13@gmail.com>
2023-01-27 10:55:50 +0900

    - Polishing designs with fonts and colors

--- SwiftUIApp/SwiftUIApp/MenuList/View/ItemRow.swift
+++ SwiftUIApp/SwiftUIApp/MenuList/View/ItemRow.swift
@@ -9,20 +9,33 @@ import SwiftUI
 
 struct ItemRow: View {
     let item : MenuItem
+    
+    let colors : [ String : Color ] = [ "D": .purple, "G" :.black, "N" : .red, "S" : .blue, "V" : .green ]
+    
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
-                .border(.indigo, width: 1)
-                .cornerRadius(5)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(.indigo,lineWidth: 2))
             
             VStack( alignment :.leading ) {
-                
+
                 Text(item.name)
                     .font(.custom("AmericanTypewriter", size: 18))
+                
                 Text("👉 $ \(item.price).00")
                     .font(.custom("AmericanTypewriter", size: 15))
                     .foregroundColor(.red)
                 
+                }
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
             }
         }
     }
