3c07506028d8fb5a78c052107c5ce3ed424c4f24
Nem Sothea <nemsothea13@gmail.com>
2023-02-22 15:49:37 +0900

    - update data

--- CoordinatorApp/CoordinatorApp/ViewController/Popup/PopupVC.swift
+++ CoordinatorApp/CoordinatorApp/ViewController/Popup/PopupVC.swift
@@ -7,16 +7,32 @@
 
 import UIKit
 
-class PopupVC : UIViewController {
-
-    @IBOutlet weak var contentLabel     : UILabel!
+class PopupVC : UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView : UITableView!
+    
     var detail                          = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.contentLabel.text = detail
+    
         // Do any additional setup after loading the view.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? PopupCell else { return PopupCell()}
+        
+        cell.infoLabel.text = detail
+
+        return cell
+    }
+    
+
+   
     
     @IBAction func tapClick(_ sender: Any) {
         self.dismiss(animated: true)
