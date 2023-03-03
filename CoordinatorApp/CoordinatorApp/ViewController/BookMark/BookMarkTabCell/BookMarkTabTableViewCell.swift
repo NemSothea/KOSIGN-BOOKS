0929ea9eccfaae7999b9a53eb98b6f6fb77df19c
Nem Sothea <nemsothea13@gmail.com>
2023-03-03 11:15:09 +0900

    - add random UIColor

--- CoordinatorApp/CoordinatorApp/ViewController/BookMark/BookMarkTabCell/BookMarkTabTableViewCell.swift
+++ CoordinatorApp/CoordinatorApp/ViewController/BookMark/BookMarkTabCell/BookMarkTabTableViewCell.swift
@@ -15,7 +15,7 @@ class BookMarkTabTableViewCell : UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-       
+        self.icon.tintColor = UIColor.random()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
