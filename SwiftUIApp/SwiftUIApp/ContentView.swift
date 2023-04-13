dc8a2b838cd1d23d24b041d02b739348e3745678
Nem Sothea <nemsothea13@gmail.com>
2023-04-13 15:30:04 +0900

    - Two-way bindings in SwiftUI

--- SwiftUIApp/SwiftUIApp/ContentView.swift
+++ SwiftUIApp/SwiftUIApp/ContentView.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        /**
+         1. Without @State we wouldn’t be able to change properties in our structs, because structs are fixed values.
+         2. Without StateObject we wouldn’t be able to create classes that stay alive for the duration of our app.
+         3. Without @EnvironmentObject we wouldn’t be able to receive shared data from elsewhere in our app.
+         4. Without ObservableObject we wouldn’t be notified when an external value changes.
+         5. Without $property two-way bindings we would need to update values by hand.
+         */
         MenuList()
     }
 }
