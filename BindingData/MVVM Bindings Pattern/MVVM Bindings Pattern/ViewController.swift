cbee2da9899139b5bdb73a5ab1472ca2f7fb9dc6
Nem Sothea <nemsothea13@gmail.com>
2023-05-31 15:23:34 +0900

    - Binding Data init

---
+++ BindingData/MVVM Bindings Pattern/MVVM Bindings Pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM Bindings Pattern
+//
+//  Created by Bizplay on 2023/05/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
