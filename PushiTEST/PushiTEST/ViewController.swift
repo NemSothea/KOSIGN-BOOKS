b3312fea0a0041a1fc33adcabff96f718070f9d4
Nem Sothea <nemsothea13@gmail.com>
2023-07-17 14:25:54 +0900

    - remove Swift Package Manager

---
+++ PushiTEST/PushiTEST/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PushiTEST
+//
+//  Created by Bizplay on 2023/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
