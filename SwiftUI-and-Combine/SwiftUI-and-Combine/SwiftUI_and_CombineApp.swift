451a0280cd4c4b5132d2f56b09e73a3b0f80d216
Nem Sothea <nemsothea13@gmail.com>
2023-07-19 19:44:42 +0900

    - create new app

---
+++ SwiftUI-and-Combine/SwiftUI-and-Combine/SwiftUI_and_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_and_CombineApp.swift
+//  SwiftUI-and-Combine
+//
+//  Created by NEMSOTHEA on 2023/07/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_and_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
