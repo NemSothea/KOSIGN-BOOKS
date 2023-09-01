de361822662a3f19eae7574e23f1a3684bf66c0e
Nem Sothea <nemsothea13@gmail.com>
2023-09-01 10:47:06 +0900

    - create sample Collection View

---
+++ CollectionViewSample/CollectionViewSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewSample
+//
+//  Created by Bizplay on 2023/09/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
